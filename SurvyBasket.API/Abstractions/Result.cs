0c6aa77a012de0ea14933bca3383690df7ef72fc
Ahmed Gamal <gemi6276@gmail.com>
2024-08-21 10:53:21 +0200

    improve Result Return Problem Details

---
+++ SurvyBasket.API/Abstractions/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace SurveyBasket.API.Abstractions
+{
+    public class Result
+    {
+        public Result( bool isSucess , Error error)
+        {
+            if ((isSucess && error != Error.None) || (!isSucess && error == Error.None))
+                throw new InvalidOperationException();
+
+            IsSuccess = isSucess;
+            Error = error;
+            
+        }
+
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error = default!;
+
+        public static Result Success()
+            => new(true, Error.None);
+
+        public static Result Failure(Error error)
+            => new(false, error);
+
+        public static Result<TValue> Success<TValue>(TValue value)
+            => new(value, true, Error.None);
+
+        public static Result<TValue> Failure<TValue>(Error error)
+            => new(default!, false, error);
+
+
+    }
+
+
+    public class Result<TValue> : Result
+    {
+        private readonly TValue? _value;
+        public Result(TValue value ,  bool isSucess, Error error) : base(isSucess, error)
+        {
+            _value = value;
+        }
+
+        public TValue Value
+            => IsSuccess ? _value! : throw new InvalidOperationException("Failure Result Cant have Value");
+    
+    }
+}
---
+++ SurvyBasket.API/Extensions/ResultExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyBasket.API.Abstractions;
+
+namespace SurveyBasket.API.Extensions
+{
+    public static class ResultExtension
+    {
+        public static ObjectResult ToProblem(this Result result , int statusCode)
+        {
+            if (result.IsSuccess)
+                throw new InvalidOperationException("Cant convert success result to problem");
+
+
+            var problem =  Results.Problem(statusCode: statusCode);
+            var problemDetails = problem.GetType().GetProperty(nameof(ProblemDetails))!.GetValue(problem) as ProblemDetails;
+          
+            problemDetails!.Extensions = new Dictionary<string, object?>
+            {
+                {
+                    "errors" , new[] {result.Error}
+                }
+            };
+
+            return new ObjectResult(problemDetails);
+        }
+    }
+}
